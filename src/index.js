e9dc5aec2632c01e05af7ad1f57090b40fa0e223
Mikaël Bouchez <bouchez@ekino.com>
2016-11-21 19:44:13 +0100

    Start step 3 Webpack talks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React      from 'react';
-import ReactDOM   from 'react-dom';
-import HelloWorld from './components/HelloWorld';
-
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('root'),
-);
+console.log('Hello World !');
