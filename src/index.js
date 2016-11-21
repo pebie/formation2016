30c25ea437497f4044f2825f157a9089ce23f1ce
Mikaël Bouchez <bouchez@ekino.com>
2016-11-21 23:46:20 +0100

    Correction step3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import ReactDOM from 'react-dom';
+import React    from 'react';
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root'),
+  );
+}
+
+setInterval(tick, 1000);
