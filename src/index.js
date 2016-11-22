6da1d58f77d6ec27ed213907524f55f6dae11662
Mikaël Bouchez <bouchez@ekino.com>
2016-11-23 00:05:11 +0100

    Extracting Component end

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import ReactDOM from 'react-dom';
 import React    from 'react';
 
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar author={props.author} />
+      <div className="UserInfo-name">
+        {props.author.name}
+      </div>
+    </div>
+  );
+}
+UserInfo.propTypes = {
+  author: React.PropTypes.shape({
+    name: React.PropTypes.string,
+    avatarUrl: React.PropTypes.string,
+  }),
+};
+
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.author.avatarUrl}
+      alt={props.author.name}
+    />
+  );
+}
+Avatar.propTypes = {
+  author: React.PropTypes.shape({
+    name: React.PropTypes.string,
+    avatarUrl: React.PropTypes.string,
+  }),
+};
+
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo author={props.author} />
       <div className="Comment-text">
         {props.text}
       </div>
@@ -36,7 +60,7 @@ const element = (<Comment
   author={{
     name: 'Mikaël',
     avatarUrl: 'https://fr.gravatar.com/userimage/31654435/861121bab44174b542f625e11d796268.png?size=200' }}
-  text={'I\'ve made this component at this date :'}
+  text={'I wrote a super comment on my blog'}
   date={new Date().toLocaleString()}
 />);
 ReactDOM.render(element, document.getElementById('root'));
