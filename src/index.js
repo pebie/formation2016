19d1bec73c1a425a0f4af9273e82b185116fcdae
Mikaël Bouchez <bouchez@ekino.com>
2016-11-22 23:52:43 +0100

    Extracting Components start

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 import ReactDOM from 'react-dom';
 import React    from 'react';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {props.date}
+      </div>
     </div>
   );
-  ReactDOM.render(
-    element,
-    document.getElementById('root'),
-  );
 }
-
-setInterval(tick, 1000);
-
-function Welcome(props) {
-  return <div>Hello {props.name}</div>;
-}
-Welcome.propTypes = {
-  name: React.PropTypes.string,
+Comment.propTypes = {
+  author: React.PropTypes.shape({
+    name: React.PropTypes.string,
+    avatarUrl: React.PropTypes.string,
+  }),
+  text: React.PropTypes.string,
+  date: React.PropTypes.string,
 };
 
-const element = <Welcome name="Mikaël" />;
-
-ReactDOM.render(
-  element,
-  document.getElementById('welcome'),
-);
+const element = (<Comment
+  author={{
+    name: 'Mikaël',
+    avatarUrl: 'https://fr.gravatar.com/userimage/31654435/861121bab44174b542f625e11d796268.png?size=200' }}
+  text={'I wrote a super comment on my blog'}
+  date={new Date().toLocaleString()}
+/>);
+ReactDOM.render(element, document.getElementById('root'));
